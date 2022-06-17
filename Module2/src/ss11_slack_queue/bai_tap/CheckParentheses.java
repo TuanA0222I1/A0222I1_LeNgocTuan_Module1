b158b590b3de75b7def57665f946d3d891ee66d7
Le Ngoc Tuan <cumeo154@gmail.com>
2022-06-17 18:52:42 +0700

    update

--- Module2/src/ss11_slack_queue/bai_tap/CheckParentheses.java
+++ Module2/src/ss11_slack_queue/bai_tap/CheckParentheses.java
@@ -5,38 +5,55 @@ import java.util.Stack;
 
 
 public class CheckParentheses {
-    public static boolean isParenthesesIsCorrectType(String str) { // only ()
-        Stack<Character> expressionLeft = new Stack<>();
-        Stack<Character> expressionRight = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == '(') {
-                expressionLeft.push(str.charAt(i));
-            } else if (str.charAt(i) == ')') {
-                expressionRight.push(str.charAt(i));
-            }
-        }
-        return expressionLeft.size() != 0 && expressionLeft.size() == expressionRight.size();
-    }
 
-    public static boolean isAllParentheses(String str) { // {[()]}
+    public static boolean isAllParentheses(String s) { // {[()]}
         Stack<Character> stack = new Stack<>();
-        for (Character character : str.toCharArray()) {
+        for (Character character : s.toCharArray()) {
             if (character == '(' || character == '{' || character == '[') {
                 stack.push(character);
                 continue;
             }
-            if (character == ')' && (stack.pop() != '(' || stack.empty())) return false;
-            if (character == '}' && (stack.pop() != '{' || stack.empty())) return false;
-            if (character == ']' && (stack.pop() != '[' || stack.empty())) return false;
+            switch (character) {
+                case ')':
+                    if (stack.empty()) return false;
+                    if (stack.pop() != '(') return false;
+
+                    break;
+                case '}':
+                    if (stack.empty()) return false;
+                    if (stack.pop() != '{') return false;
+
+                    break;
+                case ']':
+                    if (stack.empty()) return false;
+                    if (stack.pop() != '[') return false;
+                    break;
+            }
         }
         return stack.isEmpty();
     }
+
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character character : s.toCharArray()) {
+            if (character == '(' || character == '{' || character == '[') {
+                stack.push(character);
+                continue;
+            }
+            if ((character == ')') && (stack.empty() || stack.pop() != '(')) return false;
+            if ((character == '}') && (stack.empty() || stack.pop() != '{')) return false;
+            if ((character == ']') && (stack.empty() || stack.pop() != '[')) return false;
+        }
+        return stack.isEmpty();
+    }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input expression: ");
         String expression = scanner.nextLine();
-        if (isParenthesesIsCorrectType(expression)) {
+        if (isValid(expression)) {
             System.out.println(expression + " Correct parentheses");
         } else {
             System.out.println(expression + " NOT Correct parentheses");
