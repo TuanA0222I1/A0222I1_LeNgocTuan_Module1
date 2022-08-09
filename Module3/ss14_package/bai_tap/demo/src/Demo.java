a2bee6019dea4850724586f176f1388d167c9359
Le Ngoc Tuan <cumeo154@gmail.com>
2022-08-09 23:52:24 +0700

    s14

---
+++ Module3/ss14_package/bai_tap/demo/src/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(DegreeEqual.resultCalculator(1,2,3));
+        System.out.println(DegreeEqual.resultCalculator(2,-7,3));
+        System.out.println(DegreeEqual.resultCalculator(1,-4,4));
+    }
+}
